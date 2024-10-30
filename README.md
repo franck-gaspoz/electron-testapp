baefa4ed8512b469f324beed69ebc215d10aad40
franck gaspoz <franck.gaspoz@gmail.com>
2024-10-30 04:07:58 +0100

    setup update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![logo electron](https://camo.githubusercontent.com/c6a5b63f3d61c2932806c52e77e0650015d890a182ebc9fc977e4d0cbe826d95/68747470733a2f2f656c656374726f6e6a732e6f72672f696d616765732f656c656374726f6e2d6c6f676f2e737667)
+
 # electron test app
 ___
 from @ [https://www.electronjs.org/docs/latest/tutorial/tutorial-first-app](https://www.electronjs.org/docs/latest/tutorial/tutorial-first-app)
--- main.js
+++ main.js
@@ -1,37 +1,79 @@
-const { app, Tray, Menu, nativeImage, BrowserWindow } = require('electron')
+const {
+    app,
+    Tray,
+    Menu,
+    nativeImage,
+    BrowserWindow,
+    nativeTheme,
+    ipcMain
+} = require('electron')
+
+const path = require('node:path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        titleBarStyle: 'visible',
+        title: "Electron App",
+
+        frame: false,
+        titleBarStyle: 'hidden',
+        titleBarOverlay: false,
+
+        fullscreen: false,
         fullScreenable: 'true',
-        frame: true,
         menuBarVisible: false,
         shadow: true,
         resizable: true,
-        visibleOnAllWorkspaces: false,
-        title: "electron app",
         movable: true,
+        visibleOnAllWorkspaces: false,
+        /*
         width: 1700,
         height: 956,
         minWidth: 1700,
-        minHeight: 956
-    })
-    win.setMenuBarVisibility(false)
-    win.center()
-    win.fullScreen = true
+        minHeight: 956,
+        */
+        width: 800,
+        height: 600,
+        minWidth: 400,
+        minHeight: 200,
 
-    //win.loadFile('index.html')
-    win.loadFile('C:\\Users\\franc\\source\\repos\\MovieDbAssistant\\MovieDbAssistant.App\\bin\\Debug\\net8.0-windows10.0.22621.0\\output\\ok.ru\\index.html')
+        //skipTaskbar: true,
+
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    app.setUserTasks([])
+    const icon = nativeImage.createFromPath('icon.png')
+    win.setIcon(icon)
+    //win.setTitleBarOverlay({ color: 'red', 'symbolColor': 'blue', height: 16 })
+    win.center()
+
+    win.loadFile('index.html')
+    //win.loadFile('C:\\Users\\franc\\source\\repos\\MovieDbAssistant\\MovieDbAssistant.App\\bin\\Debug\\net8.0-windows10.0.22621.0\\output\\ok.ru\\index.html')
 }
 
 const createTray = () => {
     const icon = nativeImage.createFromPath('icon.png')
     const tray = new Tray(icon)
-    tray.setToolTip('Movie Db Assistant')
-    tray.setTitle('Movie Db Assistant')
+    tray.setToolTip('Electron App tooltip')
+    tray.setTitle('Electron App title')
+
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Item1', type: 'radio' },
+        { label: 'Item2', type: 'radio' },
+        { label: 'Item3', type: 'radio', checked: true },
+        { label: 'Item4', type: 'radio' }
+    ])
+    tray.setContextMenu(contextMenu)
+}
+
+const setupIpc = () => {
+    ipcMain.handle('ping', () => 'pong')
 }
 
 app.whenReady().then(() => {
+    setupIpc()
+    nativeTheme.themeSource = "dark"
     createTray()
     createWindow()
 })
